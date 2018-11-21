19fee91659bb1a18ca2a507a16492aa58e95b7bd
Mara Kortenkamp <mara@cinderella.de>
2018-11-21 23:13:04 +0100

    solutions for project 4 from group 2 (Lucas, Joana, Shipra, Mara) added

---
+++ projects/project_4/test_poisson_group2.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+from poisson import create_laplacian_2d
+
+
+def test_laplacian_2d():
+    laplacian = create_laplacian_2d(3, 3, 1, 1, pbc=True)
+    np.testing.assert_almost_equal(
+        laplacian,
+        np.array([[-4.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                  [1.0, -4.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+                  [1.0, 1.0, -4.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                  [1.0, 0.0, 0.0, -4.0, 1.0, 1.0, 1.0, 0.0, 0.0],
+                  [0.0, 1.0, 0.0, 1.0, -4.0, 1.0, 0.0, 1.0, 0.0],
+                  [0.0, 0.0, 1.0, 1.0, 1.0, -4.0, 0.0, 0.0, 1.0],
+                  [1.0, 0.0, 0.0, 1.0, 0.0, 0.0, -4.0, 1.0, 1.0],
+                  [0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, -4.0, 1.0],
+                  [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, -4.0]]) * (3 / 1)**2)
+    laplacian = create_laplacian_2d(3, 3, 1, 1, pbc=False)
+    np.testing.assert_almost_equal(
+        laplacian,
+        np.array([[-4.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                  [1.0, -4.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, 1.0, -4.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                  [1.0, 0.0, 0.0, -4.0, 1.0, 0.0, 1.0, 0.0, 0.0],
+                  [0.0, 1.0, 0.0, 1.0, -4.0, 1.0, 0.0, 1.0, 0.0],
+                  [0.0, 0.0, 1.0, 0.0, 1.0, -4.0, 0.0, 0.0, 1.0],
+                  [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -4.0, 1.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, -4.0, 1.0],
+                  [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, -4.0]]) * (3 / 1)**2)
+
+
+@pytest.mark.parametrize('nx,ny,lx,ly,pbc,exception', [
+    (1, 1, 1, 1, True, ValueError),
+    (3, 2, 1, 1, True, ValueError),
+    (3, 3, -1, 1, True, ValueError),
+    ('hello', 3, 1, 1, True, TypeError),
+    (3, 3, None, 1, True, TypeError),
+    (3, 3, 1, 1, 'hello', TypeError)])
+def test_laplacian_2d_exceptions(nx, ny, lx, ly, pbc, exception):
+    with pytest.raises(exception):
+        create_laplacian_2d(nx, ny, lx, ly, pbc=pbc)
